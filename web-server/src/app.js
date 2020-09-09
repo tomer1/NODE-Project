8450b3be7ffb5350aa61beac88487a38d11dba3d
tomer <tomer.morisi@gmail.com>
2020-09-09 22:37:24 +0300

    add resources

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,14 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
-//app.com
-//app.com/help
-//app.com/about
-app.get('', (req, res) => {
-  res.send('Hello express!');
-});
-
-app.get('/help', (req, res) => {
-  res.send('Help page');
-});
-
-app.get('/about', (req, res) => {
-  res.send('about page');
-});
+const publicDirectory = path.join(__dirname, '../public/');
+app.use(express.static(publicDirectory));
 
 app.get('/weather', (req, res) => {
-  res.send('weather page');
+  res.send({ forecast: "it's snowing", location: 'Israel' });
 });
+
 app.listen(3000, () => {
   console.log('Server is up in port 3000');
 });
